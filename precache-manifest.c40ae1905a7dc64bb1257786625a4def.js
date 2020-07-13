89826b608e7dace385d1dab70165dcaf264c58ae
kesava <kesava@localhost.localdomain>
2020-07-13 19:02:58 +0530

    Updates

---
+++ precache-manifest.c40ae1905a7dc64bb1257786625a4def.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d036d852661fad68d3f115ba01c321",
+    "url": "/newton_front_end/shopping-cart/index.html"
+  },
+  {
+    "revision": "e9591eb5ae3dd276e794",
+    "url": "/newton_front_end/shopping-cart/static/css/main.9942ba39.chunk.css"
+  },
+  {
+    "revision": "430eb6af239d6e130f4b",
+    "url": "/newton_front_end/shopping-cart/static/js/2.b2fc5582.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newton_front_end/shopping-cart/static/js/2.b2fc5582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9591eb5ae3dd276e794",
+    "url": "/newton_front_end/shopping-cart/static/js/main.bae2aa5d.chunk.js"
+  },
+  {
+    "revision": "2c3227d073013b95ab6a",
+    "url": "/newton_front_end/shopping-cart/static/js/runtime-main.0bc6b1ec.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/newton_front_end/shopping-cart/precache-manifest.c40ae1905a7dc64bb1257786625a4def.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/newton_front_end/shopping-cart/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
