10e2ce5d218795cee757ef6d303b021186484bb2
kesavamuthu <kesavamuthu77@gmail.com>
2020-07-27 07:02:46 +0530

    form got initated centered upload button

--- react-with-hooks/src/components-excel/initialUploadScreen.js
+++ react-with-hooks/src/components-excel/initialUploadScreen.js
@@ -2,19 +2,25 @@ import React, { useState } from "react";
 import ExcelInput from "./excel";
 import Warning from "./warning";
 import ShowPassedData from "./dispaly";
-import { Jumbotron } from "react-bootstrap";
+import { Jumbotron, Container, Row, Col } from "react-bootstrap";
 
 function InitialLoad(props) {
   const [show, setShow] = useState(true);
   if (props.show)
     return (
-      <Jumbotron className={"jumbotron-what"}>
-        {show ? (
-          <ExcelInput show onRead={helper} refer={props.refer} />
-        ) : (
-          <Warning onClick={setShow} />
-        )}
-      </Jumbotron>
+      <Container className={"my-auto"}>
+        <Row style={{ marginTop: "12rem" }}>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Jumbotron className={"jumbotron-what"}>
+              {show ? (
+                <ExcelInput show onRead={helper} refer={props.refer} />
+              ) : (
+                <Warning onClick={setShow} />
+              )}
+            </Jumbotron>
+          </Col>
+        </Row>
+      </Container>
     );
   else return <ShowPassedData data={props.data} />;
 
